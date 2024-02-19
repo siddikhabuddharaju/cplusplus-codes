716fc7a8bbecde7c83d6955328ee0c555915b8bb
siddikhabuddharaju <150932314+siddikhabuddharaju@users.noreply.github.com>
2024-02-19 22:33:19 +0530

    boxArea.h

---
+++ boxArea.h
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+void boxArea(float length,float width,float height)
+{
+    cout<<"Area of Box :- "<<(2*(length*width)+(width*height)+(height*length))<<endl;
+}
